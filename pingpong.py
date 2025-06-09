be5f554ae5902473a59e578bf665713804777ee0
MaxFrance88 <max_france@mail.ru>
2025-06-09 20:28:34 +0300

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,71 @@
+import pygame
+import sys
+
+
+pygame.init()
+
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Пинг-Понг")
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+FPS = 60
+PADDLE_WIDTH, PADDLE_HEIGHT = 10, 100
+BALL_SIZE = 20
+SPEED = 5
+
+
+left_paddle = pygame.Rect(30, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+right_paddle = pygame.Rect(WIDTH - 40, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+
+ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
+ball_dx, ball_dy = SPEED, SPEED
+
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and left_paddle.top > 0:
+        left_paddle.y -= SPEED
+    if keys[pygame.K_s] and left_paddle.bottom < HEIGHT:
+        left_paddle.y += SPEED
+    if keys[pygame.K_UP] and right_paddle.top > 0:
+        right_paddle.y -= SPEED
+    if keys[pygame.K_DOWN] and right_paddle.bottom < HEIGHT:
+        right_paddle.y += SPEED
+    ball.x += ball_dx
+    ball.y += ball_dy
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_dy = -ball_dy
+
+    
+    if ball.colliderect(left_paddle) or ball.colliderect(right_paddle):
+        ball_dx = -ball_dx
+
+
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball.x = WIDTH // 2 - BALL_SIZE // 2
+        ball.y = HEIGHT // 2 - BALL_SIZE // 2
+        ball_dx *= -1
+
+
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, left_paddle)
+    pygame.draw.rect(screen, WHITE, right_paddle)
+    pygame.draw.ellipse(screen, WHITE, ball)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
